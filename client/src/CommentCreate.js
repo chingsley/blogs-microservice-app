24436009e4d1905cfbd8a85b052e859f15d292af
chingsley <eneja.kc@gmail.com>
2021-09-15 07:32:57 +0100

    - Deploy the frontend react to kubernetes

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -8,7 +8,7 @@ function CommentCreate({ postId }) {
     event.preventDefault();
 
     if (content === "") return;
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
       content,
     });
 
